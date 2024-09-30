e92af4dc8c56ba77083a4b6840877ca32e7cadc9
Junsik <junsik8337@gmail.com>
2024-09-30 15:40:19 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'EOS ToDoList',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        fontFamily: "Pretendard", // 수정: 폰트 패밀리 이름만 사용
+        useMaterial3: true,
       ),
       home: const MyHomePage(),
     );
@@ -26,52 +28,65 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: const Text('EOS ToDoList'),
-        leading: const Icon(Icons.check_box_outlined),
+          backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+          title: Text('EOS Todolist'),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('assets/images/eos_logo.png'),
+          )
       ),
-      body: Container(
-        height: 200,
-        color: Colors.lightGreen.withOpacity(0.3),
-        padding: const EdgeInsets.all(25),
-        child: Row(
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  width: 10,
-                  color: Colors.grey,
-                ),
-              ),
-              child: const Center(
-                child: Icon(
-                  Icons.person,
-                  size: 100,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            const SizedBox(width: 10),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text('EOS'),
-                Text(
-                  '김준식',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            color: Colors.lightGreen.withOpacity(0.3),
+            padding: EdgeInsets.all(25),
+            child: Row(
+              children: [
+                Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      width: 10,
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(70),
+                  ),
+                  child: Center(
+                    child: Image.asset('assets/images/eos_logo.png'),
                   ),
                 ),
-                Text('몽통 부회장'),
+                SizedBox(
+                  width: 35,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('EOS'),
+                    Text(
+                      '김준식',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700, // 수정: FontWeight 사용
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text('최강한화 내년엔 비상하리'),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+
+          Expanded(
+            child: Container(
+              color: Color(0xFFA4C639).withOpacity(0.1),
+            ),
+          ),
+        ],
       ),
     );
   }
